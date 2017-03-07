133f46baaab85c5ca6d88f8088a9ce6c16c3e980
KRAUSE1 <krausedev@gmail.com>
2017-03-07 15:37:28 -0600

    c

---
+++ Lab7_361/src/editor/Employee.java
@@ -0,0 +1,22 @@
+package editor;
+
+public class Employee {
+
+	String lastName;
+	String firstName;
+	String Phone;
+	String Department;
+	
+	public Employee(String LastName, String FirstName, String Phone, String Department){
+		lastName = LastName;
+		firstName = FirstName;
+		this.Phone = Phone;
+		this.Department = Department;
+	}
+	
+	public String toString(){
+		String str="";
+		
+		return str;
+	}
+}
