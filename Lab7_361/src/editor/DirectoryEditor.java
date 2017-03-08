8e3c3b830ab123ef3a90d4d388cede4141878a0b
dishenwen <dwen@uwm.edu>
2017-03-08 13:58:24 -0600

    Push

--- Lab7_361/src/editor/DirectoryEditor.java
+++ Lab7_361/src/editor/DirectoryEditor.java
@@ -1,5 +1,11 @@
 package editor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
@@ -7,19 +13,50 @@ import com.google.gson.reflect.TypeToken;
 
 
 public class DirectoryEditor {
-	
+static DirectoryProxy DP=new DirectoryProxy();
 
+	
+	
 	public static void main (String[] args)
 	{
 		Scanner std=new Scanner(System.in);
-		DirectoryProxy DP=new DirectoryProxy();
-		
-		
-		
-		System.out.println("Please enter commends");
-		while(true)
+		System.out.println("File(f) or Input(i)");
+		String Ys=std.nextLine();
+		if(Ys.equalsIgnoreCase("f"))
 		{
-			String str=std.nextLine();
+			File file =new File("File");
+			String path =  file.getAbsolutePath();
+			std.close();
+			readFromFile(path);
+			
+		}
+		else{
+			
+			ReadFconsole(std);
+		}
+		
+		
+		
+	}
+
+	public static void readFromFile(String file){//use RaceData/testBuff.txt
+		try(Scanner buff = new Scanner(new FileInputStream(file))){	
+			String currentLine;
+			while((buff.hasNext())){
+				currentLine = buff.nextLine();
+				//currentLine = buff.next();
+				commandExec(currentLine,buff);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+	public static void commandExec(String str,Scanner std)
+	{
+		
+			
 			switch(str){
 			case("CLR"): 
 				DP.Clear();
@@ -27,10 +64,14 @@ public class DirectoryEditor {
 			case("ADD"):
 				str=std.nextLine();
 			//Keep update until see END
-			while(str.equals("END")){  
+			List<Employee> Emlist=new ArrayList<>();
+			while(!str.equals("END")){  
+				
 			    String [] Data = str.split(" ");
-				DP.add(Data[0],Data[1],Data[2],Data[3]);
+				Employee ed=new Employee(Data[1], Data[0], Data[3], Data[2]);
+						Emlist.add(ed);
 			}
+			DP.add(Emilst);
 			//need add update here
 			break;
 			case("PRINT"):
@@ -40,33 +81,27 @@ public class DirectoryEditor {
 				System.out.println("Not a valid command\n");
 				break;
 			
-			
-			
-			
-			
-			
-			
-			}
-			
-			
-			
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
+			}		
+	
 	}
 	
-	
-	
-	
-	
-	
-	
+			
+	public static void ReadFconsole(Scanner std)
+	{
+		String command;
+		System.out.println("Please enter commends");
+		while(std.hasNext())
+		{
+			command = std.nextLine();
+			commandExec(command,std);
+			System.out.println("Enter commands");
+		}
+
+	}
 }
+	
+	
+		
+	
+	
+
